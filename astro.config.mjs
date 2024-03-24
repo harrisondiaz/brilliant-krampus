9e479ce0d15852aef5c6bfa56a7f077f5e9ac57a
Harrison Diaz <harryjhoi01@gmail.com>
2024-03-24 11:34:34 -0500

    Revert "Update Astro configuration and dependencies"
    This reverts commit dbc406ed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
-  adapter: vercel()
-});
+export default defineConfig({});
